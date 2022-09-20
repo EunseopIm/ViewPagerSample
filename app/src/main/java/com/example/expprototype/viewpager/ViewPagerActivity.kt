763844bcd35c6d544ea942d3835228ee94d06f26
eunseop.im <eunseop.im@samsung.com>
2022-09-20 13:39:01 +0900

    -

--- app/src/main/java/com/example/expprototype/viewpager/ViewPagerActivity.kt
+++ app/src/main/java/com/example/expprototype/viewpager/ViewPagerActivity.kt
@@ -1,10 +1,16 @@
 package com.example.expprototype.viewpager
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.expprototype.databinding.ActivityViewPagerBinding
+import java.lang.Math.abs
+
 
 class ViewPagerActivity : AppCompatActivity() {
 
@@ -20,6 +26,48 @@ class ViewPagerActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initViewPager()
+    }
+
+    private val pageTransformer = ViewPager2.PageTransformer { page, position ->
+
+        if (position == 0f || position == 1f || position == -1f
+            || position == 0.5f || position == -0.5f) {
+            Log.v(">>>", "@# Test---[${binding.viewPager.currentItem}] position($position)")
+        }
+
+        page.cameraDistance = 20000f
+        page.cameraDistance = 10000f
+
+        if (position < -1){     // [-Infinity,-1)
+            // This page is way off-screen to the left.
+            page.alpha = 0f
+
+        }
+        else if (position <= 0){    // [-1,0]
+
+            page.alpha = 1f
+            page.pivotX = page.width.toFloat()
+            page.rotationY = ((90 * abs(position)))
+        }
+        else if (position <= 1){    // (0,1]
+
+            page.alpha = 1f
+            page.pivotX = 0f
+            page.rotationY = (-90 * abs(position))
+
+        }
+        else{    // (1,+Infinity]
+            // This page is way off-screen to the right.
+            page.alpha = 0f
+        }
+
+        if (abs(position) <= 0.5){
+            page.scaleY = Math.max(.4f,1- kotlin.math.abs(position));
+        }
+        else if (abs(position) <= 1){
+            page.scaleY = Math.max(.4f, kotlin.math.abs(position));
+
+        }
     }
 
     private fun initViewPager() {
@@ -44,6 +92,46 @@ class ViewPagerActivity : AppCompatActivity() {
                 }
             })
             viewPager.offscreenPageLimit = fragments.size
+            //viewPager.setPageTransformer(pageTransformer)
+        }
+
+        //binding.viewPager.currentItem = fragments.size-1
+        //animateViewPager(binding.viewPager, 10, 3000)
+    }
+
+    private var animFactor = 0
+    private val animator = ValueAnimator()
+
+    private fun animateViewPager(pager: ViewPager2, offset: Int, delay: Int) {
+        if (!animator.isRunning) {
+            animator.removeAllUpdateListeners()
+            animator.removeAllListeners()
+            //Set animation
+            animator.setIntValues(0, -offset)
+            animator.duration = delay.toLong()
+            animator.repeatCount = 1
+            animator.repeatMode = ValueAnimator.RESTART
+            animator.addUpdateListener { animation ->
+                val value = animFactor * animation.animatedValue as Int
+                if (!pager.isFakeDragging) {
+                    pager.beginFakeDrag()
+                }
+                pager.fakeDragBy(value.toFloat())
+            }
+            animator.addListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationStart(animation: Animator) {
+                    animFactor = 1
+                }
+
+                override fun onAnimationEnd(animation: Animator) {
+                    pager.endFakeDrag()
+                }
+
+                override fun onAnimationRepeat(animation: Animator) {
+                    animFactor = -1
+                }
+            })
+            animator.start()
         }
     }
 }
