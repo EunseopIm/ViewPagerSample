af6701af37b67fce5a54c973474b4764d291b7d0
eunseop.im <eunseop.im@samsung.com>
2022-09-20 10:00:10 +0900

    - 화면이동

--- app/src/main/java/com/example/expprototype/MainActivity.kt
+++ app/src/main/java/com/example/expprototype/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.expprototype
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.expprototype.databinding.ActivityMainBinding
@@ -16,10 +17,16 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnGallery.setOnClickListener {
 
+            Intent(this, GalleryViewActivity::class.java).apply {
+                startActivity(this)
+            }
         }
 
         binding.btnViewPager.setOnClickListener {
 
+            Intent(this, ViewPagerActivity::class.java).apply {
+                startActivity(this)
+            }
         }
     }
 }
