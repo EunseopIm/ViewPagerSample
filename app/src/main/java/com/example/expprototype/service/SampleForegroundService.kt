49562a22fa2161776173941af46a694b9668f307
eunseop.im <eunseop.im@samsung.com>
2022-09-22 19:24:57 +0900

    - Foreground Service 기능추가

--- app/src/main/java/com/example/expprototype/service/SampleForegroundService.kt
+++ app/src/main/java/com/example/expprototype/service/SampleForegroundService.kt
@@ -1,17 +1,25 @@
 package com.example.expprototype.service
 
 import android.app.*
+import android.app.ActivityManager.RunningAppProcessInfo
+import android.app.usage.UsageEvents
+import android.app.usage.UsageStatsManager
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.expprototype.MainActivity
 import com.example.expprototype.R
 import com.example.expprototype.service.ServiceActivity.Companion.ACTION_STOP
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SampleForegroundService : Service() {
 
@@ -39,8 +47,14 @@ class SampleForegroundService : Service() {
         //START_REDELIVER_INTENT: 이름에서 알겠듯이 마지막 Intent를 onStartCommand의 인자로 다시 전달해줍니다. 즉각적인 반응이 필요한 파일 다운로드 서비스 같은 곳에 적합합니다.
 
         generateForegroundNotification()
+        checkActivity()
 
         return START_NOT_STICKY
+    }
+
+    var stopFlag = false
+    override fun onDestroy() {
+        super.onDestroy()
     }
 
     private fun generateForegroundNotification() {
@@ -83,6 +97,142 @@ class SampleForegroundService : Service() {
             notification = builder.build()
             startForeground(mNotificationId, notification)
         }
+    }
 
+    var id = 1001
+    private fun generateForegroundNotification2() {
+
+        Log.v(">>>", "@# generateForegroundNotification2")
+        val intentMainLanding = Intent(this, MainActivity::class.java)
+        val notifyPendingIntent =
+            PendingIntent.getActivity(this, 0, intentMainLanding, PendingIntent.FLAG_MUTABLE)
+
+        val builder: NotificationCompat.Builder
+        val mNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val CHANNEL_ID = getString(R.string.app_name)
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                getString(R.string.app_name),
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.setShowBadge(false)
+            mNotificationManager.createNotificationChannel(channel)
+            builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            builder.setDefaults(Notification.DEFAULT_LIGHTS or Notification.DEFAULT_SOUND)
+                .setAutoCancel(true)
+                .setVibrate(longArrayOf(0L))
+            builder.setNumber(0)
+        } else {
+            builder = NotificationCompat.Builder(this)
+            builder.setAutoCancel(true)
+        }
+
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentTitle("title")
+            .setContentText("message")
+            .setContentIntent(notifyPendingIntent)
+        mNotificationManager.notify(id++, builder.build())
+    }
+
+    private val runnable = {
+
+        try {
+
+            val manager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+            val info = manager.getRunningTasks(1)
+            val componentName = info[0].topActivity
+            val topActivityName = componentName!!.shortClassName.substring(1)
+
+            generateForegroundNotification2()
+            Log.v(">>>", "@# Check : ${topActivityName}")
+            Log.v(">>>", "@# Check2 : ${getForegroundActivity()}")
+            Log.v(">>>", "@# Check3 : ${getForegroundApp(applicationContext)}")
+            Log.v(">>>", "@# Check4 : ${getForegroundApp2(applicationContext)}")
+
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+
+        checkActivity()
+    }
+
+    var first = false
+    private fun checkActivity() {
+
+        if (stopFlag) return
+        val secondDelay = 1000 * 15
+        val firstDelay = 1000 * 15
+        var delay = if (first) secondDelay else firstDelay
+
+        Handler(Looper.getMainLooper()).postDelayed(runnable, 1000 * 15)
+    }
+
+    private fun getForegroundActivity(): String? {
+
+        val mActivityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        if (mActivityManager.getRunningTasks(1) == null) {
+            Log.e(">>>", "running task is null, ams is abnormal!!!")
+            return null
+        }
+        val mRunningTask = mActivityManager.getRunningTasks(1)[0]
+        if (mRunningTask == null) {
+            Log.e(">>>", "failed to get RunningTaskInfo")
+            return null
+        }
+        //String activityName =  mRunningTask.topActivity.getClassName();
+        return mRunningTask.topActivity!!.packageName
+    }
+
+    private fun getForegroundApp(context: Context): String? {
+
+        val am = context.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val lr = am.runningAppProcesses ?: return null
+        for (ra in lr) {
+            if (ra.importance == RunningAppProcessInfo.IMPORTANCE_VISIBLE
+                || ra.importance == RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+            ) {
+                return ra.processName
+            }
+        }
+        return null
+    }
+
+    private val sdf = SimpleDateFormat("(dd) HH:mm:ss")
+
+    var isInit = true
+    private fun getForegroundApp2(context: Context): String? {
+
+        Log.v(">>>", "getForegroundApp2")
+        val usageStatsManager = context.getSystemService(USAGE_STATS_SERVICE) as UsageStatsManager
+        val ts = System.currentTimeMillis()
+        val queryUsageStats = usageStatsManager.queryUsageStats(UsageStatsManager.INTERVAL_BEST, 0, ts)
+        val usageEvents = usageStatsManager.queryEvents(if (isInit) 0 else ts - 5000, ts) ?: return null
+        val event = UsageEvents.Event()
+        var lastEvent: UsageEvents.Event? = null
+
+        Log.v(">>>", "getForegroundApp2 size : ${usageEvents.getNextEvent(event)}")
+        while (usageEvents.getNextEvent(event)) {
+            // if from notification bar, class name will be null
+            if (event.packageName == null || event.className == null) {
+                continue
+            }
+
+            if (lastEvent == null || lastEvent.timeStamp < event.timeStamp) {
+                lastEvent = event
+            }
+
+            val calendar = Calendar.getInstance()
+            calendar.timeInMillis = event.timeStamp
+
+            val lastCalendar = Calendar.getInstance()
+            lastCalendar.timeInMillis = lastEvent.timeStamp
+
+            if (event.eventType == UsageEvents.Event.ACTIVITY_RESUMED) {
+                Log.v(">>>", "#@ Name[${event.packageName}], Time[${sdf.format(calendar.time)}] LastTime[${sdf.format(lastCalendar.time)}]")
+            }
+        }
+        return lastEvent?.packageName
     }
 }
--- app/src/main/java/com/example/expprototype/service/ServiceActivity.kt
+++ app/src/main/java/com/example/expprototype/service/ServiceActivity.kt
@@ -1,36 +1,58 @@
 package com.example.expprototype.service
 
+import android.Manifest
 import android.app.ActivityManager
+import android.app.AppOpsManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.os.Process
+import android.provider.Settings
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.expprototype.BuildConfig
 import com.example.expprototype.databinding.ActivityServiceBinding
 
+
 class ServiceActivity : AppCompatActivity() {
+
+    companion object{
+        const val  ACTION_STOP = "${BuildConfig.APPLICATION_ID}.stop"
+    }
 
     private val binding: ActivityServiceBinding by lazy {
         ActivityServiceBinding.inflate(layoutInflater)
+    }
+
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
+        return mode == AppOpsManager.MODE_ALLOWED
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        if (!checkForPermission()) {
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        } else {
+            /*val usageStats = getAppUsageStats()
+            showAppUsageStats(usageStats)*/
+        }
+
         binding.btnStart.setOnClickListener {
 
-            Log.v(">>>", "@# Start")
             startService(Intent(this, SampleForegroundService::class.java))
             updateTextStatus()
         }
 
         binding.btnStop.setOnClickListener {
 
-            Log.v(">>>", "@# Stop")
             val intentStop = Intent(this, SampleForegroundService::class.java)
             intentStop.action = ACTION_STOP
             startService(intentStop)
@@ -52,11 +74,11 @@ class ServiceActivity : AppCompatActivity() {
     }
 
 
+    @SuppressWarnings("deprecation")
     private fun isMyServiceRunning(serviceClass: Class<*>): Boolean {
 
         try {
-            val manager =
-                getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+            val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
             for (service in manager.getRunningServices(
                 Int.MAX_VALUE
             )) {
@@ -68,11 +90,5 @@ class ServiceActivity : AppCompatActivity() {
             return false
         }
         return false
-    }
-
-
-
-    companion object{
-        const val  ACTION_STOP = "${BuildConfig.APPLICATION_ID}.stop"
     }
 }
